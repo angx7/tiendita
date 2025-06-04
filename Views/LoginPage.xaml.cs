680a0164f081f2e7128063638c0ce12ff0cdba83
angx7 <angelmirocu@gmail.com>
2025-06-03 18:13:48 -0600

    hi

--- Views/LoginPage.xaml.cs
+++ Views/LoginPage.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using _411_tiendita.services;
+using _411_tiendita.ViewModels;
 
 namespace _411_tiendita.Views;
 
@@ -11,5 +13,6 @@ public partial class LoginPage : ContentPage
     public LoginPage()
     {
         InitializeComponent();
+        BindingContext = new LoginViewModel(new AuthService(), this);
     }
 }
